4dcd7580cc6239e7a0b27843b33332f927a20b37
Benjamin Patch <ben.patch@immutable.com>
2023-10-09 12:06:48 +1100

    Deployment scripts

---
+++ script/DeployChildContracts.s.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: Apache 2.0
+pragma solidity ^0.8.21;
+
+import {Script, console2} from "forge-std/Script.sol";
+
+import {ChildERC20Bridge} from "../src/child/ChildERC20Bridge.sol";
+import {ChildAxelarBridgeAdaptor} from "../src/child/ChildAxelarBridgeAdaptor.sol";
+import {ChildERC20} from "../src/child/ChildERC20.sol";
+
+// TODO update private key usage to be more secure: https://book.getfoundry.sh/reference/forge/forge-script#wallet-options---raw
+
+contract DeployChildContracts is Script {
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address childGateway = vm.envAddress("CHILD_GATEWAY_ADDRESS");
+        address childGasService = vm.envAddress("CHILD_GAS_SERVICE_ADDRESS"); // Not yet used.
+        string memory childRpcUrl = vm.envString("CHILD_RPC_URL");
+
+        vm.createSelectFork(childRpcUrl);
+        vm.startBroadcast(deployerPrivateKey);
+
+        ChildERC20 childTokenTemplate = new ChildERC20();
+        childTokenTemplate.initialize(address(123), "TEMPLATE", "TPT", 18);
+
+        ChildERC20Bridge childBridge = new ChildERC20Bridge();
+
+        ChildAxelarBridgeAdaptor childBridgeAdaptor = new ChildAxelarBridgeAdaptor(
+            childGateway, // child gateway
+            address(childBridge) // child bridge
+        );
+        // TODO make sure we set bridge adaptors everywhere
+
+        // rootBridge.initialize(address(rootBridgeAdaptor), address(456), address(789));
+
+        vm.stopBroadcast();
+
+        console2.log("====ADDRESSES====");
+        console2.log("Child ERC20 Bridge: %s", address(childBridge));
+        console2.log("Child Axelar Bridge Adaptor: %s", address(childBridgeAdaptor));
+        console2.log("childTokenTemplate: %s", address(childTokenTemplate));
+    }
+}
