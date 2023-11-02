a898c90a71b009ab96806770d1466b8fce574379
wcgcyx <wcgcyx@gmail.com>
2023-11-02 14:08:44 +1000

    Update 8_root_initialisation.js

--- bootstrap/8_root_initialisation.js
+++ bootstrap/8_root_initialisation.js
@@ -52,7 +52,7 @@ async function run() {
 
     receipt = null;
     while (receipt == null) {
-        receipt = await rootProvider.getTransactionReceipt(childTemplate.deployTransaction.hash)
+        receipt = await rootProvider.getTransactionReceipt(resp.hash)
         await delay(1000);
     }
     console.log(receipt);
