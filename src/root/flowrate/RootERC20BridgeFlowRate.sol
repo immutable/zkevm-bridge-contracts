7e2232e5aaea03a6b7aba386207c1b84ca483627
Craig M <craig.b.macgregor@gmail.com>
2023-11-17 17:08:09 +1300

    Update src/root/flowrate/RootERC20BridgeFlowRate.sol
    Co-authored-by: Ermyas Abebe <ermyast@gmail.com>

--- src/root/flowrate/RootERC20BridgeFlowRate.sol
+++ src/root/flowrate/RootERC20BridgeFlowRate.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache 2.0
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "./FlowRateDetection.sol";
 import "./FlowRateWithdrawalQueue.sol";
