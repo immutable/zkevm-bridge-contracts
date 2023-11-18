13899568968834e22df718026c7efa4ac0553532
wcgcyx <wcgcyx@gmail.com>
2023-11-19 09:59:58 +1000

    Cleanup

--- axelar-local-dev/scripts/libs/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-require('./rootRequire');
-
-module.exports = {
-    ...require('./start'),
-    ...require('./deploy'),
-    ...require('./execute'),
-    ...require('./utils'),
-};
