97c4d0fd7a8ede7e07076ab09220f3266a22df55
wcgcyx <wcgcyx@gmail.com>
2023-11-18 21:04:14 +1000

    Update rootchain_setup.js

--- localdev/rootchain_setup.js
+++ localdev/rootchain_setup.js
@@ -9,12 +9,16 @@ async function main() {
     let rootRPCURL = helper.requireEnv("ROOT_RPC_URL");
     let rootChainID = helper.requireEnv("ROOT_CHAIN_ID");
     let rootDeployerKey = helper.requireEnv("ROOT_DEPLOYER_SECRET");
+    let axelarDeployerKey = helper.requireEnv("AXELAR_ROOT_EOA_SECRET");
 
     // Get root provider.
     let rootProvider = new ethers.providers.JsonRpcProvider(rootRPCURL, Number(rootChainID));
 
-    // Get deployer key on the root chain.
+    // Get deployer wallet on the root chain.
     let rootDeployer = new ethers.Wallet(rootDeployerKey, rootProvider);
+
+    // Get axelar wallet on the root chain.
+    let axelarDeployer = new ethers.Wallet(axelarDeployerKey, rootProvider);
     
     // Generate a key to deploy IMX contract and fund ROOT_DEPLOYER.
     let admin = ethers.Wallet.createRandom().connect(rootProvider);
@@ -45,8 +49,15 @@ async function main() {
     })
     await helper.waitForReceipt(resp.hash, rootProvider);
 
+    // Transfer 100 ETH to axelar deployer
+    resp = await admin.sendTransaction({
+        to: axelarDeployer.address,
+        value: ethers.utils.parseEther("100.0"),
+    })
+
     console.log("Root deployer now has " + ethers.utils.formatEther(await IMX.balanceOf(rootDeployer.address)) + " IMX.");
     console.log("Root deployer now has " + ethers.utils.formatEther(await rootProvider.getBalance(rootDeployer.address)) + " ETH.");
+    console.log("Root axelar now has " + ethers.utils.formatEther(await rootProvider.getBalance(axelarDeployer.address)) + " ETH.");
 
     console.log("Finished setting up on root chain.");
 }
