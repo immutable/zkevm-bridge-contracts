919e0fc7eb3dd594e784e748aa91dee17ac62984
Craig M <craig.b.macgregor@gmail.com>
2023-11-20 12:21:27 +1300

    Update src/interfaces/root/flowrate/IFlowRateWithdrawalQueue.sol
    Co-authored-by: Zoraiz Mahmood <35128199+rzmahmood@users.noreply.github.com>

--- src/interfaces/root/flowrate/IFlowRateWithdrawalQueue.sol
+++ src/interfaces/root/flowrate/IFlowRateWithdrawalQueue.sol
@@ -22,7 +22,7 @@ interface IFlowRateWithdrawalQueueEvents {
 }
 
 interface IFlowRateWithdrawalQueueErrors {
-    // // A withdrawal was being processed, but the index is outside of the array.
+    // A withdrawal was being processed, but the index is outside of the array.
     error IndexOutsideWithdrawalQueue(uint256 lengthOfQueue, uint256 requestedIndex);
 
     // A withdrawal was being processed, but the withdrawal is not yet available.
