86295319301573918784abc98c8b2cf4992b9441
wcgcyx <wcgcyx@gmail.com>
2023-11-21 12:03:56 +1000

    Enable CI

---
+++ scripts/localdev/ci.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+./start.sh &
+
+# Try at most 300 seconds
+counter=1
+while [ $counter -le 300 ]
+do
+    echo "Waiting for chain and axelar setup... ${counter}"
+    SKIP_WAIT_FOR_CONFIRMATION=true SKIP_MULTISIG_CHECK=true node ../bootstrap/2_deployment_validation.js > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        sleep 1
+        ((counter++))
+        continue
+    fi
+    break
+done
+
+./deploy.sh
+npx mocha --require mocha-suppress-logs ../e2e/
--- scripts/localdev/deploy.sh
+++ scripts/localdev/deploy.sh
@@ -2,21 +2,42 @@
 
 # Verify deployment
 SKIP_WAIT_FOR_CONFIRMATION=true SKIP_MULTISIG_CHECK=true node ../bootstrap/2_deployment_validation.js
+if [ $? -ne 0 ]; then
+    exit 1;
+fi
 
 # Deploy child contracts
 SKIP_WAIT_FOR_CONFIRMATION=true node ../bootstrap/3_child_deployment.js
+if [ $? -ne 0 ]; then
+    exit 1;
+fi
 
 # Deploy root contracts
 SKIP_WAIT_FOR_CONFIRMATION=true node ../bootstrap/4_root_deployment.js
+if [ $? -ne 0 ]; then
+    exit 1;
+fi
 
 # Initialise child contracts
 SKIP_WAIT_FOR_CONFIRMATION=true node ../bootstrap/5_child_initialisation.js
+if [ $? -ne 0 ]; then
+    exit 1;
+fi
 
 # IMX Burning
 SKIP_WAIT_FOR_CONFIRMATION=true node ../bootstrap/6_imx_burning.js
+if [ $? -ne 0 ]; then
+    exit 1;
+fi
 
 # IMX Rebalancing
 SKIP_WAIT_FOR_CONFIRMATION=true node ../bootstrap/7_imx_rebalancing.js
+if [ $? -ne 0 ]; then
+    exit 1;
+fi
 
 # Initialise root contracts
 SKIP_WAIT_FOR_CONFIRMATION=true node ../bootstrap/8_root_initialisation.js
+if [ $? -ne 0 ]; then
+    exit 1;
+fi
